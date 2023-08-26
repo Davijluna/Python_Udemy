27556ba2eb290cb28bade195d94b319757db4a25
davi <davijluna@gmail.com>
2023-08-26 19:13:44 -0300

    feat: aula 1 /trabalhando com search findall sub e compile

--- aula1.py
+++ aula1.py
@@ -2,4 +2,19 @@
 
 # DOCUMENTAÇÃO DO MODULO "RE"
 # https://docs.python.org/3/library/re.html
+# https://docs.python.org/3/howto/regex.html#regex-howto
+
 import re
+
+# findall search sub
+# compile
+
+string = 'Este é um teste de expressões regulares.'
+print(re.search(r'testes', string))
+print(re.findall(r'teste', string))
+print(re.sub(r'teste', 'ABCD', string))
+
+regexp = re.compile(r'teste')
+print(regexp.search(string))
+print(regexp.findall(string))
+print(regexp.sub('DEF', string))
