fac464f9b180998ecdbb29103d5d7bc301c96b51
davi <davijluna@gmail.com>
2023-10-18 13:34:52 -0300

    feat: instalando novas ferramentas para python e utilizando arquivo para teste

--- aula1.py
+++ aula1.py
@@ -3,18 +3,18 @@
 # DOCUMENTAÇÃO DO MODULO "RE"
 # https://docs.python.org/3/library/re.html
 # https://docs.python.org/3/howto/regex.html#regex-howto
+lista = [1,2,3]
+# import re
 
-import re
+# # findall search sub
+# # compile
 
-# findall search sub
-# compile
+# string = 'Este é um teste de expressões regulares.'
+# print(re.search(r'testes', string))
+# print(re.findall(r'teste', string))
+# print(re.sub(r'teste', 'ABCD', string))
 
-string = 'Este é um teste de expressões regulares.'
-print(re.search(r'testes', string))
-print(re.findall(r'teste', string))
-print(re.sub(r'teste', 'ABCD', string))
-
-regexp = re.compile(r'teste')
-print(regexp.search(string))
-print(regexp.findall(string))
-print(regexp.sub('DEF', string))
+# regexp = re.compile(r'teste')
+# print(regexp.search(string))
+# print(regexp.findall(string))
+# print(regexp.sub('DEF', string))
